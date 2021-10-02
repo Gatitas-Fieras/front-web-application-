5ae5be28ec32e008d07ab86a049a0080d6f224cc
SebTou08 <sebaas14toulier@gmail.com>
2021-10-02 03:35:49 -0500

    refactor: add one parameter to user

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserOutput } from 'src/app/core/models/outputs/userOutput';
+import { UserApiService } from 'src/app/core/services/user-api.service';
+import { SharedsService } from 'src/app/shared/shareds.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  description: string | undefined;
+  user :UserOutput | undefined;
+
+  constructor(private shared: SharedsService, private userApi : UserApiService) {
+
+   }
+
+
+   getUserById(): void {
+      this.userApi.getUserById(this.shared.getId()).subscribe((response: any) => {
+        this.user = response.content;
+      })
+   }
 
   ngOnInit(): void {
+    this.getUserById();
+    this.description = this.user?.description;
   }
 
 }
